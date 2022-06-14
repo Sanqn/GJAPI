7a12214458db6a5a8b58966909aa324cb9a3cb89
Sanqn <sandisvert@gmail.com>
2022-06-15 00:17:58 +0300

    Deployo

--- pers/models.py
+++ pers/models.py
@@ -5,7 +5,7 @@ class NewPerson(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
-    work_experience = models.CharField(max_length=200)
+    work_experience = models.TextField()
 
     def __str__(self):
         return self.first_name
