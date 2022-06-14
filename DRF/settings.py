2214ff266507ef78c9a88ce43afa7ee95b9a2d9b
Sanqn <sandisvert@gmail.com>
2022-06-14 22:27:52 +0300

    Deployo

--- DRF/settings.py
+++ DRF/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'DRF.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': dj_database_url.config(conn_max_age=600)
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # # 'default': dj_database_url.config(conn_max_age=600)
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 # DATABASES = {
 #     'default': {
