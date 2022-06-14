c13f9b2ba2572deff83bfe2b036dc7ad7575309a
Sanqn <sandisvert@gmail.com>
2022-06-14 23:20:24 +0300

    Deployo

--- DRF/settings.py
+++ DRF/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-# import django_heroku
+import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,7 +136,7 @@ STATIC_URL = 'static/'
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 # import os
 # import psycopg2
 #
